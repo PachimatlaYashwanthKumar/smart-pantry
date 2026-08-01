21292e9367040aa3c06e20d5c45e8c98f662d5b7
Pachimatla Yashwanth Kumar <pachimatlayashwanthkumar@gmail.com>
2026-08-01 16:09:01 +0530

    Add Health check endpoint

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,12 @@
 import express from "express";
 import cors from "cors";
+import routes from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-  res.json({
-    message: "Smart Pantry API is running 🚀",
-  });
-});
+app.use("/api/v1", routes);
 
 export default app;
