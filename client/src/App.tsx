4b3ecc033fb8089c33e0572cbfc0d38d08765588
Pachimatla Yashwanth Kumar <pachimatlayashwanthkumar@gmail.com>
2026-08-01 13:40:51 +0530

    tailwindcss installed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <div>
-      <h1>🥫 Smart Pantry</h1>
-      <p>Welcome to Smart Pantry.</p>
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="rounded-xl bg-white p-8 shadow-lg">
+        <h1 className="text-4xl font-bold text-green-600">
+          🥫 Smart Pantry
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Tailwind CSS is working successfully!
+        </p>
+      </div>
     </div>
   );
 }
