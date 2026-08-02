5a57042f0ab9a22f765f9399cc1c4f18aafb4a11
Pachimatla Yashwanth Kumar <pachimatlayashwanthkumar@gmail.com>
2026-08-02 22:15:13 +0530

    update

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,22 @@
 import express from "express";
 import cors from "cors";
+
 import routes from "./routes";
+import { errorHandler } from "./middleware/error.middleware";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.use("/api/v1", routes);
 
-export default app;
-
-import { errorHandler } from "./middleware/error.middleware";
-
-// ...
-
 app.use(errorHandler);
+
+export default app;
